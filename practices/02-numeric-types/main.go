38615b9356c7440964dff6aa57ffb7bb3070f5fd
Jorge Luis Sanchez Labastida <jsanchez.labastida@gmail.com>
2026-09-04 22:32:45 -0600

    feat: improve the readability to the code

--- practices/02-numeric-types/main.go
+++ practices/02-numeric-types/main.go
@@ -4,24 +4,24 @@ import "fmt"
 
 func main() {
 	var product string = "Tempered Glass 10mm"
-	var qyt int8 = 3
-	var unit_price float32 = 1250.50
+	var quantity int8 = 3
+	var unitPrice float32 = 1250.50
 	var discount float32 = 10.00
 	var tax float32 = 16.00
 
-	var subtotal float32 = float32(qyt) * unit_price
-	var discount_amount float32 = subtotal / discount
-	var subtotal_after_discount float32 = subtotal - discount_amount
-	var tax_amount float32 = (subtotal_after_discount * tax) / 100
-	var final_total float32 = subtotal_after_discount + tax_amount
+	var subtotal float32 = float32(quantity) * unitPrice
+	var discountAmount float32 = subtotal * discount / 100
+	var subtotalAfterDiscount float32 = subtotal - discountAmount
+	var taxAmount float32 = subtotalAfterDiscount * tax / 100
+	var finalTotal float32 = subtotalAfterDiscount + taxAmount
 
 	fmt.Println("Product: ", product)
-	fmt.Println("Quantity: ", qyt)
-	fmt.Printf("Unit price: $%.2f\n", unit_price)
+	fmt.Println("Quantity: ", quantity)
+	fmt.Printf("Unit price: $%.2f\n", unitPrice)
 	fmt.Println("")
 	fmt.Printf("Subtotal: $%.2f\n", subtotal)
-	fmt.Printf("Discount: $%.2f\n", discount_amount)
-	fmt.Printf("After discount: $%.2f\n", subtotal_after_discount)
-	fmt.Printf("Taxes: $%.2f\n", tax_amount)
-	fmt.Printf("Final total: $%.2f\n", final_total)
+	fmt.Printf("Discount: $%.2f\n", discountAmount)
+	fmt.Printf("After discount: $%.2f\n", subtotalAfterDiscount)
+	fmt.Printf("Tax: $%.2f\n", taxAmount)
+	fmt.Printf("Final total: $%.2f\n", finalTotal)
 }
